9f7a6f661d9742964bfa1559b8b4c457ef76f74c
Snoopy20111 <snoopy20111@gmail.com>
2022-10-25 22:47:00 -0400

    Fun with sliding menus
    Main Menu now slides left and right to go to Options and the eventual Race Details screen

--- Scripts/AutoResize_ColorRect.gd
+++ Scripts/AutoResize_ColorRect.gd
@@ -4,17 +4,10 @@ extends ColorRect
 # Size in pixels / units to go over the window size
 export var bleed_size: int = 50
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	resize()
+	get_tree().get_root().connect("size_changed", self, "viewport_resized")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	resize()
-
-
-func resize():
+func viewport_resized():
 	var viewport_size = get_viewport_rect().size
 	rect_size = viewport_size + Vector2(bleed_size,bleed_size)
--- Scripts/MenuHorizontal.gd
+++ Scripts/MenuHorizontal.gd
@@ -5,36 +5,81 @@ extends Control
 onready var windowWidth: int = get_viewport_rect().size.x
 export var extraWidth: int = 50
 onready var totalWidth: int = windowWidth + extraWidth
+var animSpeed: float = 1.0
 
 enum MOVEMENT_STATE {LEFT, RIGHT, PAUSE}
 var currentMovementState = MOVEMENT_STATE.PAUSE
-var transitionDirection = 1
-export var animSpeed = 50
 
-onready var Main = $Main
-onready var Options = $Options
-onready var RaceDetails = $RaceDetails
+var targetNodeGroup
+onready var Main:= $Main
+var mainLocation: float = 0
+onready var Options:= $Options
+onready var optionsLocation: float = totalWidth
+onready var RaceDetails:= $RaceDetails
+onready var raceDetailsLocation: float = -totalWidth
+
+onready var canStopInCenterTimer:= $CanStopInCenterTimer
+var canStopInCenter: bool = false
 
 func _ready():
-	# Main.rect_position.x
-	Options.rect_position.x = totalWidth
-	RaceDetails.rect_position.x = -totalWidth
+	#Place Options and RaceDetails to the right and left respectively
+	set_positions()
+	
+	#Attach the viewport resized event to function viewport_resized()
+# warning-ignore:return_value_discarded
+	get_tree().get_root().connect("size_changed", self, "viewport_resized")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Check current viewport size
+	match (currentMovementState):
+		MOVEMENT_STATE.LEFT:
+			if (self.rect_position.x > totalWidth):
+				currentMovementState = MOVEMENT_STATE.PAUSE
+				self.rect_position.x = totalWidth
+			elif ((self.rect_position.x < 50) and (self.rect_position.x > -50) and (canStopInCenter == true)):
+				print("true")
+				currentMovementState = MOVEMENT_STATE.PAUSE
+				self.rect_position.x = 0
+				canStopInCenter = false
+			else:
+				self.rect_position.x += animSpeed * delta * totalWidth
+				
+		MOVEMENT_STATE.RIGHT:
+			if (self.rect_position.x < -totalWidth):
+				currentMovementState = MOVEMENT_STATE.PAUSE
+				self.rect_position.x = -totalWidth
+			elif ((self.rect_position.x < 50) and (self.rect_position.x > -50) and (canStopInCenter == true)):
+				print("true")
+				currentMovementState = MOVEMENT_STATE.PAUSE
+				self.rect_position.x = 0
+				canStopInCenter = false
+			else:
+				self.rect_position.x -= animSpeed * delta * totalWidth
+
+
+func canStopInCenterTimer_ended():
+	canStopInCenter = true
+
+func set_positions():
+	Main.rect_position.x = mainLocation
+	Options.rect_position.x = optionsLocation
+	RaceDetails.rect_position.x = raceDetailsLocation
+
+func viewport_resized():
 	windowWidth = get_viewport_rect().size.x
 	totalWidth = windowWidth + extraWidth
-	Options.rect_position.x = totalWidth
-	RaceDetails.rect_position.x = -totalWidth
-	
-	
-	match (currentMovementState):
-		MOVEMENT_STATE.PAUSE:
-			pass
-		MOVEMENT_STATE.LEFT:
-			self.rect_position.x -= animSpeed * delta
-		MOVEMENT_STATE.RIGHT:
-			self.rect_position.x += animSpeed * delta
-	
-	
+	optionsLocation = totalWidth
+	raceDetailsLocation = -totalWidth
+	set_positions()
+	print(windowWidth)
+
+func move_left():
+	currentMovementState = MOVEMENT_STATE.LEFT
+	canStopInCenter = false
+	canStopInCenterTimer.start()
+
+func move_right():
+	currentMovementState = MOVEMENT_STATE.RIGHT
+	canStopInCenter = false
+	canStopInCenterTimer.start()
